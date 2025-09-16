3e8f67c1fa11763175c65acb02f5e917657e767b
TheDeadlyWalker <jasoncupit82@gmail.com>
2025-09-16 18:14:32 +0100

    Update Deadly_Antibiotic_Mushroom.c

--- Scripts/4_World/entities/Edible_Base/Deadly_Antibiotic_Mushroom.c
+++ Scripts/4_World/entities/Edible_Base/Deadly_Antibiotic_Mushroom.c
@@ -5,7 +5,7 @@ class Deadly_Antibiotic_Mushroom: MushroomBase
         super.OnConsume(amount, consumer);
         if (!GetGame().IsServer() || !consumer) return;
 
-        // Give the player a “cold/flu” agent
-        consumer.InsertAgent(eAgents.INFLUENZA, 50); // 50 = dose; tune to taste
+        // Give the player a “INFLUENZA”
+        consumer.InsertAgent(eAgents.INFLUENZA, 50);
     }
 };
