38466114f920d752d25bed3d96bb68f1edd6824f
TheDeadlyWalker <jasoncupit82@gmail.com>
2025-09-16 17:50:22 +0100

    Add files via upload

---
+++ Scripts/3_Game/3_Game/Liquid_Healing_Tea/constants.c
@@ -0,0 +1,4 @@
+const int Liquid_Healing_Tea				= 615;
+const int Liquid_Energy_Tea                 = 616;
+const int Liquid_Antibiotic_Tea             = 617;
+const int Liquid_Death_Tea                  = 618;
