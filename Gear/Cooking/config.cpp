e3ef2ca7fabef3230663f4f38f579fe483c4b658
TheDeadlyWalker <jasoncupit82@gmail.com>
2025-09-16 18:31:42 +0100

    Update config.cpp

--- Gear/Cooking/config.cpp
+++ Gear/Cooking/config.cpp
@@ -1,10 +1,8 @@
 class CfgPatches
 {
-	class Deadly_Textures_Cooking // Your mod name followed by the main folder name - DO NOT HAVE DUPLICATE NAMES IF YOU HAVE MULTIPLE CONFIGS
-	{
+	class Deadly_Textures_Cooking 
 		weapons[] = {};
 		requiredVersion = 0.1;
-		// Below are the required addons that this config needs specifically
 		requiredAddons[] = 
 		{
 			"DZ_Data"
@@ -32,4 +30,5 @@ class CfgVehicles
 			"Deadly_Textures\Gear\cooking\Data\Deadly_Witch_cauldron_co.paa"
 		};
 	};
+
 };
