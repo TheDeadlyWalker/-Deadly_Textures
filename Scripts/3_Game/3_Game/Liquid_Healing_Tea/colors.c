e119e6120b7977a738d6665829fc577f0a713bb5
TheDeadlyWalker <jasoncupit82@gmail.com>
2025-09-16 17:54:39 +0100

    Delete Scripts/3_Game/3_Game directory

--- Scripts/3_Game/3_Game/Liquid_Healing_Tea/colors.c
+++
@@ -1,7 +0,0 @@
-modded class Colors
-{
-	const int Liquid_Healing_Tea			= 0xCFE2F3;		// Light Blue
-	const int Liquid_Energy_Tea				= 0xFFA500;		// Orange
-	const int Liquid_Antibiotic_Tea			= 0x00FF00;		// Green
-	const int Liquid_Death_Tea				= 0xBEBEBE;		// Gray
-};
--- Scripts/3_Game/3_Game/Liquid_Healing_Tea/constants.c
+++
@@ -1,4 +0,0 @@
-const int Liquid_Healing_Tea				= 615;
-const int Liquid_Energy_Tea                 = 616;
-const int Liquid_Antibiotic_Tea             = 617;
-const int Liquid_Death_Tea                  = 618;
