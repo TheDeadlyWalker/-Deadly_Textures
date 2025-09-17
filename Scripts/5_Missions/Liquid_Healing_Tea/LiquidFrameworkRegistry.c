47d42706ec509e185815551c55198089896a9ace
Deadly_Walker <jasoncupit82@gmail.com>
2025-09-17 18:55:43 +0100

    Add files via upload

---
+++ Scripts/5_Missions/Liquid_Healing_Tea/LiquidFrameworkRegistry.c
@@ -0,0 +1,13 @@
+modded class LiquidFrameworkRegistry
+{
+	override void RegisterCustomLiquids()
+	{
+		super.RegisterCustomLiquids();
+		
+		RegisterLiquid(Liquid_Healing_Tea, "Healing Tea", Colors.Liquid_Healing_Tea);
+		RegisterLiquid(Liquid_Energy_Tea,  "Energy Tea", Colors.Liquid_Energy_Tea);
+		RegisterLiquid(Liquid_Antibiotic_Tea,  "Antibiotic Tea", Colors.Liquid_Antibiotic_Tea);
+		RegisterLiquid(Liquid_Death_Tea,  "Liquid Death", Colors.Liquid_Death_Tea);
+		RegisterLiquid(Liquid_Trip_Tea,  "Space Brew", Colors.Liquid_Trip_Tea);
+	}
+};
